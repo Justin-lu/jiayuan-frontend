eb3b6d893eb3e69a2cf41432f85e8ae63e51897b
Justin-lu <gdjyluxiaoyong@gmail.com>
2014-11-15 20:42:50 +0800

    add angular css

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -9,9 +9,11 @@ var webAppControllers = angular.module('webAppControllers',[]);
 webAppControllers.controller('homeController', function($scope) {
   // create a message to display in our view
   $scope.message = 'home page!';
+  $(".nav-header").text("首页");
 });
 
 //showController
 webAppControllers.controller('showController', function($scope) {
   $scope.message = 'show page';
+  $(".nav-header").text("团购详情");
 });
